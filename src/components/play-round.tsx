9662b612aa158f98aa54efa23ae61c19fd3fe893
Tom Randolph <thomas.randolph26@gmail.com>
2024-04-17 12:31:39 -0700

    remove unused component

--- src/components/play-round.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { inputAnswer } from "@/actions/game";
-import { Input } from "./ui/input";
-
-export const InputAnswer = ({ roundId }: { roundId: number }) => {
-  return (
-    <Input type="text" name="answer" onSubmit={() => inputAnswer(roundId)} />
-  );
-};
